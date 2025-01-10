225239a0828d457adeef53f256b1321c52e121f5
BugHunter-X <askzenai@gmail.com>
2025-01-10 21:44:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,9 @@
+function myFunction(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both arguments must be numbers');
+  }
+  return a + b;
+}
+
+console.log(myFunction(5, 5)); // Output: 10
+console.log(myFunction(5, '5')); // throws an error
